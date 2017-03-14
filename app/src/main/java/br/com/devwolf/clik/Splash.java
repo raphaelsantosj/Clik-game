c51c8e665ac649ce7ff183ac30e31647d22dc263
Raphael <raphaelsantos.j@gmail.com>
2017-03-14 20:51:33 -0300

    Shape botão redondo

---
+++ app/src/main/java/br/com/devwolf/clik/Splash.java
@@ -0,0 +1,17 @@
+package br.com.devwolf.clik;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
