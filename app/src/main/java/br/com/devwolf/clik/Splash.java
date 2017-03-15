782a76b6255c29ea991dee4747d1a5b582e61244
Raphael <raphaelsantos.j@gmail.com>
2017-03-14 21:17:13 -0300

    Shape botão redondo

--- app/src/main/java/br/com/devwolf/clik/Splash.java
+++ app/src/main/java/br/com/devwolf/clik/Splash.java
@@ -1,10 +1,15 @@
 package br.com.devwolf.clik;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
+
+    //Timer da Tela de Splash
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,22 @@ public class Splash extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity principal
+                Intent i = new Intent(Splash.this, Menu.class);
+                startActivity(i);
+
+                // Fecha esta activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
