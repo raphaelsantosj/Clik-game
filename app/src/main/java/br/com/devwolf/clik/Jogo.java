0094296d11812a519a193db8dda3a961b9c9d768
Raphael <raphaelsantos.j@gmail.com>
2017-03-17 11:05:49 -0300

    Otimização dos botões supresa

--- app/src/main/java/br/com/devwolf/clik/Jogo.java
+++ app/src/main/java/br/com/devwolf/clik/Jogo.java
@@ -1,10 +1,21 @@
+/**
+ * @version Beta
+ * @author Raphael dos Santos Jesus
+ */
+
 package br.com.devwolf.clik;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Jogo extends AppCompatActivity {
+
+    Button btnCompartilhar;
+    Button btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,34 @@ public class Jogo extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        //Metodo Compartilhar
+        btnCompartilhar = (Button) findViewById(R.id.btnCompartilhar);
+        btnCompartilhar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Here is the share content body";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent,  "COMPARTILHAR"));
+            }
+        });
+        //Fim Metodo Compartilhar
+
+        //Metodo Voltar Menu
+        btnSair = (Button) findViewById(R.id.btnSair);
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sair = new Intent(Jogo.this, Menu.class);
+                startActivity(sair);
+
+                finish();
+            }
+        });
+        //Fim metodo Voltar Menu
+
     }
 }
